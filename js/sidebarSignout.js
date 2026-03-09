aa0e933c1ba583396b2ea9224b76f120b121df12
Devpriya Kulemannege <80089198+devkulemannege@users.noreply.github.com>
2026-03-09 20:15:41 +0530

    make search result page

--- js/sidebarSignout.js
+++ js/sidebarSignout.js
@@ -1,29 +1,37 @@
-    function showOptions(event) {
-        event.stopPropagation();
-    
-        var navItem = event.target.closest('.header_right_area'); // position to be displayed 
-        var dropdown = navItem.querySelector('.dropdown-content'); // get css class for dropdown
-    
-        var isAlreadyOpen = dropdown.style.display === 'block'; 
-    
-        closeallDropdowns(); // close all dropdowns
-    
-        if (!isAlreadyOpen) {
-            dropdown.style.display = 'block'; // display only when other dropdowns are closed 
-        }
-    }
-    function signout() {
-        sessionStorage.clear();
-        window.location.href = "signin.html";
-    }
+const searchBox = document.getElementById("search_box");
 
-    function closeallDropdowns() {
-        var dropdowns = document.querySelectorAll('.dropdown-content');
-        dropdowns.forEach(function(d) {
-            d.style.display = 'none'; // set all dropdown options to hidden
-        });
+searchBox.addEventListener("keypress", function(event){
+    if (event.key === 'Enter'){
+        window.location.href = "search.html";
     }
+});
+    
+function showOptions(event) {
+    event.stopPropagation();
 
-    window.onclick = function() {
-        closeallDropdowns();
-    };
+    var navItem = event.target.closest('.header_right_area'); // position to be displayed 
+    var dropdown = navItem.querySelector('.dropdown-content'); // get css class for dropdown
+
+    var isAlreadyOpen = dropdown.style.display === 'block'; 
+
+    closeallDropdowns(); // close all dropdowns
+
+    if (!isAlreadyOpen) {
+        dropdown.style.display = 'block'; // display only when other dropdowns are closed 
+    }
+}
+function signout() {
+    sessionStorage.clear();
+    window.location.href = "signin.html";
+}
+
+function closeallDropdowns() {
+    var dropdowns = document.querySelectorAll('.dropdown-content');
+    dropdowns.forEach(function(d) {
+        d.style.display = 'none'; // set all dropdown options to hidden
+    });
+}
+
+window.onclick = function() {
+    closeallDropdowns();
+};
