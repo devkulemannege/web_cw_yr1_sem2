07ad8c72b447518f20f5d8ac2711cb5f235c4bdd
Devpriya Kulemannege <80089198+devkulemannege@users.noreply.github.com>
2026-03-16 16:12:25 +0530

    add credentials info box as confirm box

--- js/signin.js
+++ js/signin.js
@@ -3,9 +3,30 @@ function validateSignin(){
     var pswBox = document.getElementById("pswBox").value;
     var errorTextUnfilled = document.getElementById("unfilled")
     var errorTextInvalid = document.getElementById("invalid")
+
+    function forceSignin(){
+        let confirmValue = confirm(`IMPORTANT\n\nSign-in credentials have been documented in Page 3 of the Coursework Report. The reason for this approach has been justified & documented on the same page.\n
+            E-mail: elias.8645@zeroth.edu.us
+            Password: elias8645\n\nPress "Ok" to force sign-in\nPress "Cancel" to close this box.`)
+
+        if (confirmValue){
+            //create session variables in browser 
+            sessionStorage.setItem("accountStatus", "logged");
+            sessionStorage.setItem("webStatus", "unenrolled")
+            sessionStorage.setItem("programmingStatus", "unenrolled")
+            sessionStorage.setItem("databaseStatus", "unenrolled")
+            sessionStorage.setItem("mathsStatus", "unenrolled")
+
+            window.location.href = "home.html";
+        }
+    }
+
     if (email == "" || pswBox == ""){
         errorTextInvalid.style.display = "none";
         errorTextUnfilled.style.display = "block";
+
+        forceSignin()
+
         return false;
     }
     if (email == "elias.8645@zeroth.edu.us" && pswBox == "elias8645"){
@@ -22,6 +43,9 @@ function validateSignin(){
     } else {
         errorTextUnfilled.style.display = "none";
         errorTextInvalid.style.display = "block";
+        
+        forceSignin()
+
         return false;
     }
 } 
