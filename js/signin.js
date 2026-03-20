bad9428602d5346bb17ddb61c8141fd43e6afb88
Devpriya Kulemannege <80089198+devkulemannege@users.noreply.github.com>
2026-03-20 21:53:18 +0530

    get credentials from XML

--- js/signin.js
+++ js/signin.js
@@ -1,31 +1,38 @@
-function validateSignin(){
-    var email = document.getElementById("emailBox").value;
-    var pswBox = document.getElementById("pswBox").value;
-    var errorTextUnfilled = document.getElementById("unfilled")
-    var errorTextInvalid = document.getElementById("invalid")
+function validateSignin(event){
+    event.preventDefault(); // stop the default flow of the form
 
-    if (email == "" || pswBox == ""){
-        errorTextInvalid.style.display = "none";
-        errorTextUnfilled.style.display = "block";
+    fetch("js/course_data.xml")
+    .then(response => response.text())
+    .then(data => {
+        const xmlParser = new DOMParser();
+        const xml = xmlParser.parseFromString(data, "application/xml");
+        const universityEmail = xml.getElementsByTagName("email")[0].textContent
+        const actualPsw = xml.getElementsByTagName("password")[0].textContent
 
-        return false;
-    }
-    if (email == "elias.8645@zeroth.edu.us" && pswBox == "elias8645"){
+        var email = document.getElementById("emailBox").value;
+        var pswBox = document.getElementById("pswBox").value;
+        var errorTextUnfilled = document.getElementById("unfilled")
+        var errorTextInvalid = document.getElementById("invalid")
+
+        if (email == "" || pswBox == ""){
+            errorTextInvalid.style.display = "none";
+            errorTextUnfilled.style.display = "block";
+            return false;
+        }
+        if (email == universityEmail && pswBox == actualPsw){
+            // check if variable exists. if not, create session variables in browser 
+            sessionStorage.setItem("accountStatus", sessionStorage.getItem("accountStatus") || "logged");
+            sessionStorage.setItem("webStatus", sessionStorage.getItem("webStatus") || "unenrolled");
+            sessionStorage.setItem("programmingStatus", sessionStorage.getItem("programmingStatus") || "unenrolled");
+            sessionStorage.setItem("databaseStatus", sessionStorage.getItem("databaseStatus") || "unenrolled");
+            sessionStorage.setItem("mathsStatus", sessionStorage.getItem("mathsStatus") || "unenrolled");
+            window.location.href = "home.html";
+            return false;
+        } else {
+            errorTextUnfilled.style.display = "none";
+            errorTextInvalid.style.display = "block";
         
-        // check if variable exists. if not, create session variables in browser 
-        sessionStorage.setItem("accountStatus", sessionStorage.getItem("accountStatus") || "logged");
-        sessionStorage.setItem("webStatus", sessionStorage.getItem("webStatus") || "unenrolled");
-        sessionStorage.setItem("programmingStatus", sessionStorage.getItem("programmingStatus") || "unenrolled");
-        sessionStorage.setItem("databaseStatus", sessionStorage.getItem("databaseStatus") || "unenrolled");
-        sessionStorage.setItem("mathsStatus", sessionStorage.getItem("mathsStatus") || "unenrolled");
-
-        window.location.href = "home.html";
-        return false;
-    } else {
-        errorTextUnfilled.style.display = "none";
-        errorTextInvalid.style.display = "block";
-    
-
-        return false;
-    }
+            return false;
+        }
+    });
 } 
